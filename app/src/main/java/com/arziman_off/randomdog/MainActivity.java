901d608568549a4b0ba52a29ef8fe7556921ee05
ArzzzimanOff <arzimanovgyulahmed@yandex.ru>
2024-07-26 15:34:37 +0300

    добавлен скелет алгоритма работы с сетью (Запрос - Ответ)

--- app/src/main/java/com/arziman_off/randomdog/MainActivity.java
+++ app/src/main/java/com/arziman_off/randomdog/MainActivity.java
@@ -1,6 +1,7 @@
 package com.arziman_off.randomdog;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +9,57 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class MainActivity extends AppCompatActivity {
+    private static final String BASED_URL = "https://dog.ceo/api/breeds/image/random";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+        generateDog();
+    }
+
+    private void generateDog() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL(BASED_URL);
+                    HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+                    InputStream inputStream = urlConnection.getInputStream();
+                    InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                    StringBuilder data = new StringBuilder();
+                    String result;
+
+                    do {
+                        result = bufferedReader.readLine();
+                        if (result != null){
+                            data.append(result);
+                        }
+                    } while (result != null);
+
+                    Log.d("MainActivity", data.toString());
+                } catch (Exception e){
+                    Log.d("MainActivity", e.toString());
+                }
+            }
+        }).start();
     }
 }
